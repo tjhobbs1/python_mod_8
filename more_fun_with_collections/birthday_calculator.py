ca9bae89ecda36700422db67bb4c721e91d9417a
Ty Hobbs <tjhobbs1@dmacc.edu>
2019-10-15 04:22:49 -0500

    Test Commit

--- more_fun_with_collections/birthday_calculator.py
+++ more_fun_with_collections/birthday_calculator.py
@@ -1,2 +1,24 @@
-def half_birthdy():
-    pass
+import datetime
+import calendar
+
+
+def half_birthday(date):
+    """
+    This function takes in a birthday an then determines if it is a leap years and calculates the what the half
+    birthday should be.
+    :param date: Is the passed in birthday.
+    :return:  The half birthday.
+    """
+    if calendar.isleap(date.year):              # Test to see if the year is a leap year.
+        half_birthday_date = date + datetime.timedelta(183)
+        half_birthday_date = half_birthday_date.strftime("%B %d")
+        return half_birthday_date
+    else:
+        half_birthday_date = date + datetime.timedelta(182.5)
+        half_birthday_date = half_birthday_date.strftime("%B %d")
+        return half_birthday_date
+
+
+if __name__ == '__main__':
+    birthday = datetime.date(2000, 1, 19)
+    print(half_birthday(birthday))
